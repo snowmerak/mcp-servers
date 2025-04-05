b07a02cdea86ff1841d7e3cc99d93fb4f48f98d3
snowmerak <snowmerak@outlook.com>
2025-04-05 12:38:59 +0900

    refactor RawQueryTool to static class and add ExecuteRawQuery method for executing SQL queries

--- PostgresRawTool/RawQueryTool.cs
+++ PostgresRawTool/RawQueryTool.cs
@@ -1,11 +1,32 @@
+using System.ComponentModel;
 using ModelContextProtocol.Server;
 using Npgsql;
 
 namespace PostgresRawTool;
 
 [McpServerToolType]
-public class RawQueryTool
+public static class RawQueryTool
 {
-    private readonly NpgsqlConnection _connection;
-    private readonly string _connectionString = System.Environment.GetEnvironmentVariable("PG_CONNECTION_STRING") ?? "Host=localhost:5432;Username=postgres;Password=postgres;Database=postgres";
+    private static readonly string ConnectionString = System.Environment.GetEnvironmentVariable("PG_CONNECTION_STRING") ?? "Host=localhost:5432;Username=postgres;Password=postgres;Database=postgres";
+
+    [McpServerTool, Description("Executes a raw SQL query and returns the result as a value list.")]
+    public static async Task<List<object>> ExecuteRawQuery(string query)
+    {
+        await using var dataSource = NpgsqlDataSource.Create(ConnectionString);
+
+        var connection = await dataSource.OpenConnectionAsync();
+
+        await using var command = dataSource.CreateCommand(query);
+        await using var reader = await command.ExecuteReaderAsync();
+
+        var result = new List<object>();
+        while (await reader.ReadAsync())
+        {
+            for (var i = 0; i < reader.FieldCount; i++)
+            {
+                result.Add(reader.GetValue(i));
+            }
+        }
+        return result;
+    }
 }
